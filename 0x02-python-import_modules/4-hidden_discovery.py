87d7d3415c00a0277c026f1336c68a517fa496b6
EngineerDorothy <110990192+EngineerDorothy@users.noreply.github.com>
2022-12-06 23:47:09 +0300

    Delete 0x02-python-import_modules directory
    Python - import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-        from hidden_4 import *
-            ls = dir()
-                for name in ls:
-                            if name[:2] != "__":
-                                            print(name)
